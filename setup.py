33fcc347fcc6eb8fecb689f30bc5ccc75ace36f4
WDW <boa3465@gmail.com>
2021-11-02 20:54:31 +0900

    setup.py 수정, dlls 추가

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 import setuptools
-from glob import glob
 
 with open("README.md", "rt", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyAutoMaker", # Replace with your own username
-    version="0.1.3",
-    author="WDW",
-    author_email="boa9448@naver.com",
-    description="자동화를 위한 패키지",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/boa9448/PyAutoMaker",
-    classifiers=[
+    name = "PyAutoMaker",
+    version = "0.1.3",
+    author = "WDW",
+    author_email = "boa9448@naver.com",
+    description = "자동화를 위한 패키지",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/boa9448/PyAutoMaker",
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows :: Windows 10",
     ],
 
-    packages=["PyAutoMaker"],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    package_data = { "PyAutoMaker" : ["dlls/*"] },
     install_requires = ["psutil" ,"keyboard", "mouse", "pywin32" ,"opencv-contrib-python"],
 
-    python_requires='>=3.6',
+    python_requires = '>=3.6',
 )
