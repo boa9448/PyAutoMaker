69891dcf8a8a49403fa46f7120d4e8754245d1d2
WDW <boa3465@gmail.com>
2021-11-05 20:31:57 +0900

    __init__.py에 모듈 가져오기 추가

--- src/PyAutoMaker/__init__.py
+++ src/PyAutoMaker/__init__.py
@@ -9,3 +9,8 @@ os.environ["PATH"] += os.pathsep + os.path.join(rootDir, "dlls")
 os.environ["DLL_FOLDER"] = os.path.join(rootDir, "dlls")
 os.environ["DNN_DATA_FOLDER"] = os.path.join(rootDir, "models")
 os.environ["IMG_DATA_FOLDER"] = os.path.join(rootDir, "imgs")
+
+from .darknet import *
+from .dd import *
+from .image import *
+from .tensor import *
