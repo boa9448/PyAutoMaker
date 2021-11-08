53275b8f3e22e6f2d97a4623ec10f8134a15e9a7
WDW <boa3465@gmail.com>
2021-11-08 17:56:53 +0900

    버전 수정

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "rt", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "PyAutoMaker",
-    version = "0.1.3",
+    version = "0.1.5",
     author = "WDW",
     author_email = "boa9448@naver.com",
     description = "자동화를 위한 패키지",
