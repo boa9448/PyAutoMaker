8cc27b5282acac66756afbd121561bd56a29f388
WDW <boa3465@gmail.com>
2021-12-19 15:02:33 +0900

    테스트용 환경 스크립트를 사용하도록 수정

--- tests/dd_test.py
+++ tests/dd_test.py
@@ -1,17 +1,9 @@
-import os
-import sys
-
-cur_dir = os.path.dirname(__file__)
-src_dir = os.path.join(cur_dir, "..", "src")
-src_dir = os.path.abspath(src_dir)
-
-sys.path.append(src_dir)
-
+import env
 from PyAutoMaker import class_dd
 
 print(class_dd.KEY_LEFT_CTRL)
 
-dd = class_dd.DD()
+dd = class_dd.DDUtil()
 dd.move(0, 0, False)
 dd.key(ord('A'))
 dd.key(class_dd.KEY_LEFT_CTRL)
