6407fb07a82c7182248b57365dd024300697330f
WDW <boa3465@gmail.com>
2021-12-20 16:08:31 +0900

    모든 포트를 열거하도록 수정

--- src/PyAutoMaker/arduino.py
+++ src/PyAutoMaker/arduino.py
@@ -9,13 +9,14 @@ from serial.tools import list_ports
 from input_abs import *
 from arduino_define import *
 
-def get_port_list(name = "USB 직렬 장치") -> str or None:
-    port_list = list_ports.comports()
-    for port in port_list:
+def get_port_list(name = "USB 직렬 장치") -> list:
+    find_port_list = list_ports.comports()
+    port_list = list()
+    for port in find_port_list:
         if name in port.description:
-            return port.device
+            port_list.append(port.device)
 
-    return None
+    return port_list
 
 def upload(port : str, arduino_dir : str = "C:\\Program Files (x86)\\Arduino") -> bool:
     os.environ["path"] = os.pathsep.join([os.environ["path"], arduino_dir])
@@ -107,10 +108,10 @@ class ArduinoUtil(AbsInput):
 
 if __name__ == "__main__":
     #포트 찾아서 인두이노 펌웨어 자동 업로드
-    upload(get_port_list(), "D:\\Program Files (x86)\\Arduino")
+    upload(get_port_list()[0], "D:\\Program Files (x86)\\Arduino")
 
     #아두이노와 시리얼 통신 시작
-    arduino = ArduinoUtil(get_port_list(), 9600)
+    arduino = ArduinoUtil(get_port_list()[0], 9600)
     time.sleep(2)
 
     arduino.key(ord('A')) #A입력
