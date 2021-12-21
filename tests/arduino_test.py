fc74732c71168c8f1606465b3472b41d5908e4ef
WDW <boa3465@gmail.com>
2021-12-21 14:25:27 +0900

    사소한 수정

--- tests/arduino_test.py
+++ tests/arduino_test.py
@@ -5,33 +5,32 @@ from win32api import GetCursorPos
 import env
 import PyAutoMaker as pam
 
-if __name__ == "__main__":
-    #포트 찾아서 인두이노 펌웨어 자동 업로드
-    pam.upload(pam.get_port_list()[0], "D:\\Program Files (x86)\\Arduino")
+#포트 찾아서 인두이노 펌웨어 자동 업로드
+pam.upload(pam.get_port_list()[0], "D:\\Program Files (x86)\\Arduino")
 
-    #아두이노와 시리얼 통신 시작
-    arduino = pam.ArduinoUtil(pam.get_port_list()[0], 9600)
-    time.sleep(2)
+#아두이노와 시리얼 통신 시작
+arduino = pam.ArduinoUtil(pam.get_port_list()[0], 9600)
+time.sleep(2)
 
-    arduino.key(ord('A')) #A입력
-    arduino.key(ord('B')) #B입력
+arduino.key(ord('A')) #A입력
+arduino.key(ord('B')) #B입력
 
-    arduino.string("abcde") #문자열 abcde 입력
+arduino.string("abcde") #문자열 abcde 입력
 
-    arduino.btn(pam.ARDUINO_BUTTON_LEFT, pam.ARDUINO_BUTTON_STATUS_PRESS)  #마우스 왼쪽 누르고 있음
-    arduino.btn(pam.ARDUINO_BUTTON_LEFT, pam.ARDUINO_BUTTON_STATUS_RELEASE)#마우스 왼쪽 땜
+arduino.btn(pam.ARDUINO_BUTTON_LEFT, pam.ARDUINO_BUTTON_STATUS_PRESS)  #마우스 왼쪽 누르고 있음
+arduino.btn(pam.ARDUINO_BUTTON_LEFT, pam.ARDUINO_BUTTON_STATUS_RELEASE)#마우스 왼쪽 땜
 
-    arduino.btn(pam.ARDUINO_BUTTON_LEFT, pam.ARDUINO_BUTTON_STATUS_PRESS)  #마우스 왼쪽 누르고 있음
-    arduino.move(100, 100, True)
-    arduino.btn(pam.ARDUINO_BUTTON_LEFT, pam.ARDUINO_BUTTON_STATUS_RELEASE)#마우스 왼쪽 땜
-    time.sleep(30)
+arduino.btn(pam.ARDUINO_BUTTON_LEFT, pam.ARDUINO_BUTTON_STATUS_PRESS)  #마우스 왼쪽 누르고 있음
+arduino.move(100, 100, True)
+arduino.btn(pam.ARDUINO_BUTTON_LEFT, pam.ARDUINO_BUTTON_STATUS_RELEASE)#마우스 왼쪽 땜
+time.sleep(30)
 
-    print("pre : ", GetCursorPos())
-    arduino.move(100, 100, True) #현재 좌표에서 100, 100만큼 이동
-    time.sleep(1)
-    print("now : ", GetCursorPos())
+print("pre : ", GetCursorPos())
+arduino.move(100, 100, True) #현재 좌표에서 100, 100만큼 이동
+time.sleep(1)
+print("now : ", GetCursorPos())
 
-    print("pre : ", GetCursorPos())
-    arduino.move(100, 100, False) #절대 좌표 100, 100으로 이동
-    time.sleep(1)
-    print("now : ", GetCursorPos())
+print("pre : ", GetCursorPos())
+arduino.move(100, 100, False) #절대 좌표 100, 100으로 이동
+time.sleep(1)
+print("now : ", GetCursorPos())
