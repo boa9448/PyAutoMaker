1eb79c69f4cdd67330b29830c1ccd2ccaccc942c
WDW <boa3465@gmail.com>
2021-12-23 14:54:51 +0900

    사소한 수정

--- tests/input_test.py
+++ tests/input_test.py
@@ -11,3 +11,6 @@ port_name = pam.get_port_list()[0]
 input_ = pam.InputUtil(pam.ArduinoUtil, (port_name, 115200))
 input_.btn(pam.BUTTON_LEFT, pam.BUTTON_STATUS_PRESS)
 input_.btn(pam.BUTTON_LEFT, pam.BUTTON_STATUS_RELEASE)
+
+input_.key(pam.KEY_LEFT_ARROW, pam.KEY_STATUS_PRESS)
+input_.key(pam.KEY_LEFT_ARROW, pam.KEY_STATUS_RELEASE)
