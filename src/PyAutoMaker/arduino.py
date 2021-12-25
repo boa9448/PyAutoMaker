6e87d14135fd9316195efbd013eb7613dab87d27
WDW <boa3465@gmail.com>
2021-12-25 12:25:56 +0900

    스케치 업로드 기본 출력을 디버그모드로 설정

--- src/PyAutoMaker/arduino.py
+++ src/PyAutoMaker/arduino.py
@@ -30,7 +30,7 @@ def user_select_dir() -> str:
 
     return select_folder
 
-def upload(port : str, arduino_dir : str = "C:\\Program Files (x86)\\Arduino", use_debug : bool = False) -> bool:
+def upload(port : str, arduino_dir : str = "C:\\Program Files (x86)\\Arduino", use_debug : bool = True) -> bool:
     arduino_bin = "arduino_debug.exe" if use_debug else "arduino.exe"
     arduino_bin_full_path = os.path.join(arduino_dir, arduino_bin)
     if not os.path.isfile(arduino_bin_full_path):
