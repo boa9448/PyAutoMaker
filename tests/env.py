c5f3622fcb3bae9961d6156b106dcd5659d44c16
WDW <boa3465@gmail.com>
2022-01-09 15:50:11 +0900

    테스트 환경 스크립트에 테스트 이미지경로 추가

--- tests/env.py
+++ tests/env.py
@@ -4,5 +4,6 @@ import sys
 cur_dir = os.path.dirname(__file__)
 src_dir = os.path.join(cur_dir, "..", "src")
 src_dir = os.path.abspath(src_dir)
+test_imgs_dir = os.path.join(cur_dir, "imgs")
 
 sys.path.append(src_dir)
