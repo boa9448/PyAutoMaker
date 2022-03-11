f5134a71c24dcf761dd541ad808b81ac6595082c
WDW <boa3465@gmail.com>
2022-03-12 02:17:46 +0900

    아두이노 소스 합침 포트 열거 함수 삭제 포트 선택 함수 작성 업로드 함수 기능 변경

--- src/PyAutoMaker/arduino.py
+++ src/PyAutoMaker/arduino.py
@@ -1,5 +1,6 @@
 import os
 import time
+from ctypes import c_byte, c_ubyte, Structure, Union
 
 
 from win32api import GetCursorPos
@@ -7,26 +8,177 @@ from serial import Serial
 from serial.tools import list_ports
 
 
-from input_abs import *
-from arduino_define import *
 from utils import user_select_dir
+from input_base import *
 
 
-def get_port_list(name : str or list = ["USB 직렬 장치", "Leonardo"]) -> list:
-    find_port_list = list_ports.comports()
-    port_list = list()
-    if type(name) == str:
-        name_list = [name]
+class KeyData(Structure):
+    _pack_ = 1
+    _fields_ = [("key_code", c_ubyte)
+                , ("key_status", c_ubyte)]
+
+
+class MouseButtonData(Structure):
+    _pack_ = 1
+    _fields_ = [("button_code", c_ubyte)
+                , ("button_status", c_ubyte)]
+
+
+class MouseMoveData(Structure):
+    _pack_ = 1
+    _fields_ = [("x", c_byte)
+                , ("y", c_byte)]
+
+
+class ArduinoData(Union):
+    _fields_ = [("key_data", KeyData)
+                , ("mouse_button_data", MouseButtonData)
+                , ("mouse_move_data", MouseMoveData)]
+
+
+class ArduinoPacket(Structure):
+    _pack_ = 1
+    _fields_ = [("opcode", c_ubyte)
+                , ("data", ArduinoData)]
+
+
+CMD_OPCODE_KEY_DATA = 1
+CMD_OPCODE_MOUSE_BUTTON = 2
+CMD_OPCODE_MOUSE_MOVE = 3
+
+ARDUINO_KEY_PRESS = 1
+ARDUINO_KEY_RELEASE = 2
+
+ARDUINO_BUTTON_LEFT = 1
+ARDUINO_BUTTON_RIGHT = 2
+ARDUINO_BUTTON_MIDDLE = 4
+
+ARDUINO_BUTTON_STATUS_PRESS = 1
+ARDUINO_BUTTON_STATUS_RELEASE = 2
+
+
+
+arduino_key_map = {KEY_LEFT_CTRL : 0x80
+                    , KEY_LEFT_SHIFT : 0x81
+                    , KEY_LEFT_ALT : 0x82
+                    , KEY_LEFT_GUI : 0x83
+                    , KEY_RIGHT_CTRL : 0x84
+                    , KEY_RIGHT_SHIFT : 0x85
+                    , KEY_RIGHT_ALT : 0x86
+                    , KEY_RIGHT_GUI : 0x87
+                    , KEY_UP_ARROW : 0xDA
+                    , KEY_DOWN_ARROW : 0xD9
+                    , KEY_LEFT_ARROW : 0xD8
+                    , KEY_RIGHT_ARROW : 0xD7
+                    , KEY_BACKSPACE : 0xB2
+                    , KEY_TAB : 0xB3
+                    , KEY_RETURN   : 0xB0
+                    , KEY_ESC : 0xB1
+                    , KEY_INSERT   : 0xD1
+                    , KEY_DELETE   : 0xD4
+                    , KEY_PAGE_UP  : 0xD3
+                    , KEY_PAGE_DOWN : 0xD6
+                    , KEY_HOME   : 0xD2
+                    , KEY_END : 0xD5
+                    , KEY_CAPS_LOCK : 0xC1
+                    , KEY_F1 : 0xC2
+                    , KEY_F2 : 0xC3
+                    , KEY_F3 : 0xC4
+                    , KEY_F4 : 0xC5
+                    , KEY_F5 : 0xC6
+                    , KEY_F6 : 0xC7
+                    , KEY_F7 : 0xC8
+                    , KEY_F8 : 0xC9
+                    , KEY_F9 : 0xCA
+                    , KEY_F10 : 0xCB
+                    , KEY_F11 : 0xCC
+                    , KEY_F12 : 0xCD
+                    , KEY_F13 : 0xF0
+                    , KEY_F14 : 0xF1
+                    , KEY_F15 : 0xF2
+                    , KEY_F16 : 0xF3
+                    , KEY_F17 : 0xF4
+                    , KEY_F18 : 0xF5
+                    , KEY_F19 : 0xF6
+                    , KEY_F20 : 0xF7
+                    , KEY_F21 : 0xF8
+                    , KEY_F22 : 0xF9
+                    , KEY_F23 : 0xFA
+                    , KEY_F24 : 0xFB}
+
+
+
+def user_select_port() -> str:
+    from PySide6.QtCore import Qt, Slot
+    from PySide6.QtWidgets import (QDialog
+                                , QApplication
+                                , QListWidget
+                                , QListWidgetItem
+                                , QPushButton
+                                , QVBoxLayout
+                                , QWidget)
+    from qt_material import apply_stylesheet
+    
+    if QApplication.instance():
+        app = QApplication.instance()
     else:
-        name_list = name
+        app = QApplication()
 
-    for port in find_port_list:
-        port_list += [port.device for name_ in name_list if name_ in port.description]
+    class PortSelectDialog(QDialog):
+        def __init__(self) -> None:
+            super().__init__()
+            self.setWindowTitle("COM 포트 선택")
 
-    return port_list
+            vlayout = QVBoxLayout()
+            w = QWidget()
+            
+            self.port_list = QListWidget()
+            self.refresh_button = QPushButton("새로고침")
+            
+            vlayout.addWidget(self.port_list)
+            vlayout.addWidget(self.refresh_button)
+
+            w.setLayout(vlayout)
+            self.setLayout(vlayout)
+
+            self.refresh_port_list()
+            self.port_list.itemDoubleClicked.connect(self.select_handler)
+            self.refresh_button.clicked.connect(self.refresh_button_handler)
+
+            self.selected_port = ""
+
+        def refresh_port_list(self) -> None:
+            self.port_list.clear()
+            for port in list_ports.comports():
+                item = QListWidgetItem()
+                item.setText(port.description)
+                item.setData(Qt.UserRole, port)
+                self.port_list.addItem(item)
+
+        @Slot()
+        def refresh_button_handler(self) -> None:
+            self.refresh_port_list()
+
+        @Slot()
+        def select_handler(self) -> None:
+            cur_item = self.port_list.selectedItems()
+            if cur_item:
+                self.selected_port = cur_item[0].data(Qt.UserRole).device
+
+            self.done(True)
+
+    apply_stylesheet(app, "dark_teal.xml")
+    dlg = PortSelectDialog()
+    if dlg.exec():
+        return dlg.selected_port
+
+    return ""
 
 
-def upload(port : str, arduino_dir : str = "C:\\Program Files (x86)\\Arduino", use_debug : bool = True) -> bool:
+def upload(port : str = None, arduino_dir : str = "C:\\Program Files (x86)\\Arduino", use_debug : bool = True) -> bool:
+    if port is None:
+        port = user_select_port()
+
     arduino_bin = "arduino_debug.exe" if use_debug else "arduino.exe"
     arduino_bin_full_path = os.path.join(arduino_dir, arduino_bin)
     if not os.path.isfile(arduino_bin_full_path):
@@ -41,72 +193,5 @@ def upload(port : str, arduino_dir : str = "C:\\Program Files (x86)\\Arduino", u
     return True if ret_code == 0 else False
 
 
-class ArduinoUtil(AbsInput):
-    MOVE_STEP = 100
-
-    def __init__(self, port : str, baudrate : int):
-        self.serial = Serial(port = port, baudrate = baudrate)
-        self.key_header = CmdHeader(CMD_START_SIGN, CMD_OPCODE_KEY_DATA)
-        self.mouse_header = CmdHeader(CMD_START_SIGN, CMD_OPCODE_MOUSE_MOVE)
-        time.sleep(2)
-
-    def __del__(self):
-        self.serial.close()
-
-    def key(self, key_code : int, key_status : int) -> int:
-        key_status = ARDUINO_KEY_PRESS if key_status == KEY_STATUS_PRESS else ARDUINO_KEY_RELEASE
-        data = KeyData(arduino_key_map.get(key_code, key_code), key_status)
-
-        data = bytes(self.key_header) + bytes(data)
-        return self.serial.write(data)
-
-    def string(self, s : str):
-        for c in s:
-            self.key_press(ord(c))
-            self.key_release(ord(c))
-            time.sleep(0.03)
-
-    def make_move_data(self, x : int , y : int, relative : bool) -> bytes:
-        cur_x, cur_y = GetCursorPos()
-
-        if relative:
-            x += cur_x
-            y += cur_y
-
-        diff_x, diff_y = cur_x - x, cur_y - y
-        move_data = bytes()
-
-        count_x = abs(diff_x // self.MOVE_STEP)
-        remainder_x = abs(diff_x % self.MOVE_STEP)
-        count_y = abs(diff_y // self.MOVE_STEP)
-        remainder_y = abs(diff_y % self.MOVE_STEP)
-        move_data = bytes()
-        
-        sign = 1 if diff_x < 0 else -1
-        for _ in range(count_x):
-            data = MouseMoveData(sign * self.MOVE_STEP, 0)
-            move_data += bytes(self.mouse_header) + bytes(data)
-
-        data = MouseMoveData(sign * remainder_x, 0)
-        move_data += bytes(self.mouse_header) + bytes(data)
-
-        sign = 1 if diff_y < 0 else -1
-        for _ in range(count_y):
-            data = MouseMoveData(0, sign * self.MOVE_STEP)
-            move_data += bytes(self.mouse_header) + bytes(data)
-
-        data = MouseMoveData(0, sign * remainder_y)
-        move_data += bytes(self.mouse_header) + bytes(data)
-
-        return move_data
-
-
-    def move(self, x : int, y : int, relative : bool):
-        data = self.make_move_data(x, y, relative)
-        self.serial.write(data)
-
-    def btn(self, button_code : int, button_status : int):
-        header = CmdHeader(CMD_START_SIGN, CMD_OPCODE_MOUSE_BUTTON)
-        data = MouseButtonData(button_code, button_status)
-
-        self.serial.write(bytes(header) + bytes(data))
+if __name__ == "__main__":
+    pass
