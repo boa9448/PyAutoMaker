8b4ae953565b0350eba5b7eacdd418a1781dbe5e
wdw <boa3465@gmail.com>
2022-03-17 16:17:09 +0900

    class dd 모듈 리펙토링

--- src/PyAutoMaker/class_dd.py
+++ src/PyAutoMaker/class_dd.py
@@ -1,20 +1,24 @@
 import os
 from ctypes import c_char_p, wintypes, windll, c_int32
 
+
 from .input_base import *
 
-class DDUtil:
-    DD_KEY_PRESS = 1 #키다운
-    DD_KEY_RELEASE = 2 #키업
-    #btn함수가 사용하는 클릭 코드
-    DD_MOUSE_LDOWN = 1 #왼쪽 마우스 버튼 다운
-    DD_MOUSE_LUP = 2 #왼쪽 마우스 버튼 업
-    DD_MOUSE_RDOWN = 4 #오른쪽 다운
-    DD_MOUSE_RUP = 8 #오른쪽 업
 
-    def __init__(self, strDLLPath = os.path.join(os.environ["DLL_FOLDER"], "DD64.dll")):
+DD_KEY_PRESS = 1 #키다운
+DD_KEY_RELEASE = 2 #키업
+#btn함수가 사용하는 클릭 코드
+DD_MOUSE_LDOWN = 1 #왼쪽 마우스 버튼 다운
+DD_MOUSE_LUP = 2 #왼쪽 마우스 버튼 업
+DD_MOUSE_RDOWN = 4 #오른쪽 다운
+DD_MOUSE_RUP = 8 #오른쪽 업
+
+
+class DDUtil:
+
+    def __init__(self, dll_path : str = os.path.join(os.environ["DLL_FOLDER"], "DD64.dll")):
         #dd 라이브러리를 불러옴
-        self._hModule = windll.LoadLibrary(strDLLPath)
+        self._hModule = windll.LoadLibrary(dll_path)
         self._DD_key = self._hModule["DD_key"] #DD_key(DD키코드, 키상태)
         self._DD_key.argtypes = (c_int32, c_int32)
         self._DD_key.restype = c_int32
@@ -44,35 +48,39 @@ class DDUtil:
         self._DD_str.restype = c_int32
 
     def __del__(self):
-        #소멸자 함수
-        #windll을 통해서 로드한 dll은 명시적으로 해제할 필요는 없음
-        #버그의 여지나 반드시 해제해야하는 라이브러리는 명시적으로 해제
-        
-        #FreeLibrary함수 객체 생성
-        FreeLibrary = windll.kernel32["FreeLibrary"]
-        #함수의 인자를 강제함
+        """FreeLibrary = windll.kernel32["FreeLibrary"]
         FreeLibrary.argtypes = (wintypes.HMODULE,)
-        #실제 함수를 호출
-        #hModule = dd의 dll객체, hModule._handle = dll의 핸들
-        FreeLibrary(self._hModule._handle)
+        FreeLibrary(self._hModule._handle)"""
+
+    def key(self, key_code : int, key_status : int) -> None:
+        key_status = DD_KEY_PRESS if key_status == KEY_STATUS_PRESS else DD_KEY_RELEASE
+        self._DD_key(self._DD_todc(key_code), key_status)
+
+    def key_press(self, key_code : int) -> None:
+        self.key(key_code, KEY_STATUS_PRESS)
+
+    def key_release(self, key_code : int) -> None:
+        self.key(key_code, KEY_STATUS_RELEASE)
 
     def btn(self, button_code: int, button_status: int):
         if button_code == BUTTON_LEFT and button_status == BUTTON_STATUS_PRESS:
-            self._DD_btn(self.DD_MOUSE_LDOWN)
+            self._DD_btn(DD_MOUSE_LDOWN)
         elif button_code == BUTTON_LEFT and button_status == BUTTON_STATUS_RELEASE:
-            self._DD_btn(self.DD_MOUSE_LUP)
+            self._DD_btn(DD_MOUSE_LUP)
         elif button_code == BUTTON_RIGHT and button_status == BUTTON_STATUS_PRESS:
-            self._DD_btn(self.DD_MOUSE_RDOWN)
+            self._DD_btn(DD_MOUSE_RDOWN)
         elif button_code == BUTTON_RIGHT and button_status == BUTTON_STATUS_RELEASE:
-            self._DD_btn(self.DD_MOUSE_RUP)
+            self._DD_btn(DD_MOUSE_RUP)
         elif button_code == BUTTON_MIDDLE and button_status == BUTTON_STATUS_PRESS:
             pass
         elif button_code == BUTTON_MIDDLE and button_status == BUTTON_STATUS_RELEASE:
             pass
 
-    def key(self, key_code : int, key_status : int):
-        key_status = self.DD_KEY_PRESS if key_status == KEY_STATUS_PRESS else self.DD_KEY_RELEASE
-        self._DD_key(self._DD_todc(key_code), key_status)
+    def btn_press(self, button_code : int) -> bool:
+        return self.btn(button_code, BUTTON_STATUS_PRESS)
+
+    def btn_release(self, button_code : int) -> bool:
+        return self.btn(button_code, BUTTON_STATUS_RELEASE)
 
     def move(self, x: int, y: int, relative: bool):
         if not relative:
