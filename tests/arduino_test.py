c5f1eb8fd32002c5ead711003ad0b9b1e53ab594
wdw <boa3465@gmail.com>
2022-03-18 14:20:48 +0900

    아두이노 테스트 코드 작성

--- tests/arduino_test.py
+++ tests/arduino_test.py
@@ -1,7 +1,8 @@
 import time
+from typing import Any
 import unittest
 
-from win32api import GetCursorPos
+import win32api
 
 import env
 import PyAutoMaker as pam
@@ -9,9 +10,26 @@ import PyAutoMaker as pam
 class TestArduinoModule(unittest.TestCase):
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName=methodName)
-        #포트 찾아서 아두이노 펌웨어 자동 업로드
-        pam.upload(pam.get_port_list()[0], "C:\\Program Files (x86)\\Arduino")
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        pam.arduino.upload()
+        return super().setUpClass()
 
     def setUp(self) -> None:
-        self.arduino = pam.ArduinoUtil(pam.get_port_list()[0], 115200)        
+        self.arduino = pam.arduino.ArduinoUtil(pam.arduino.user_select_port(), 115200)
         return super().setUp()
+
+    def test_key(self) -> None:
+        self.arduino.key_press(ord("A"))
+        self.arduino.key_release(ord("A"))
+
+    def test_move(self) -> None:
+        self.arduino.move(100, 100, False)
+        time.sleep(1)
+        cur_pos = win32api.GetCursorPos()
+        self.assertEqual(cur_pos, (100 ,100))
+
+
+if __name__ == "__main__":
+    unittest.main()
